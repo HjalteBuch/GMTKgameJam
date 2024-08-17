f1e6bc8369b1c169b877c120c5c4886d95dc51d8
mikd <mikd@itu.dk>
2024-08-17 10:32:45 +0200

    Created tilemap and water animation

---
+++ Assets/Scripts/Status.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Status : MonoBehaviour
+{
+    [SerializeField] int maxHealth = 100;
+    [SerializeField] int maxStamina = 100;
+    [SerializeField] int maxHunger = 100;
+    [SerializeField] int maxThirst = 100;
+
+    private int health;
+    private int stamina;
+    private int hunger;
+    private int thirst;
+
+    void Start()
+    {
+        health = maxHealth;
+        stamina = maxStamina;
+        hunger = maxHunger;
+        thirst = maxThirst;
+        InvokeRepeating("statusUpdate", 1f, 1f); //will run statusUpdate function once every 1 sec
+    }
+
+    void statusUpdate() {
+        hunger--;
+        thirst--;
+        if (hunger == 0 || thirst == 0) {Die();}
+    }
+
+    void Die()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
