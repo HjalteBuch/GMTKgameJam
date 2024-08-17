3e9b144e8e963af0ca2de94c72acab88b99f7e45
Alexander <AlexSmith@live.dk>
2024-08-17 16:53:19 +0200

    Created StatusBar Script

---
+++ Assets/Scripts/StatusBar.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetMaxValue(int maxValue){
+        slider.maxValue = maxValue;
+        slider.value = maxValue;
+    }
+
+    public void SetValue(int value){
+        slider.value = value;
+    }
+}
